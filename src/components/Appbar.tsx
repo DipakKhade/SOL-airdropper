46f3b8ebee48fb6a29cdc7369c84a6d0e0b8d137
= <=>
2024-08-31 12:13:48 +0530

    appbar primsa init

---
+++ src/components/Appbar.tsx
@@ -0,0 +1,10 @@
+
+export default function Apppbar(){
+
+    return<>
+    <nav className="flex justify-between border border-slate-400 h-12">
+        <div>SOL AirDropper</div>
+        <div>asd</div>
+    </nav>
+    </>
+}
