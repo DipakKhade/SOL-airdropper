494a47f80a97131ca279522e9e5657aba02192ab
= <=>
2024-09-01 11:04:50 +0530

    added a vo landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import Landing from "@/components/landing";
 
 export default function Home() {
   // https://github.com/anza-xyz/wallet-adapter/blob/master/APP.md
   return (
     <main>
-      <Hero />
+      {/* <Hero /> */}
+      <Landing/>
     </main>
   );
 }
--- src/components/MenuButtons.tsx
+++ src/components/MenuButtons.tsx
@@ -1,12 +1,14 @@
+'use client';
 import React from "react"
-import {Button} from '../components/ui/button'
+import { Button } from '../components/ui/button'
+import { useRouter } from "next/navigation"
 
 export default function MenuButtons(){
-
+    const router = useRouter()
     return<>
-<div className="space-x-4 pt-12">
-    <Button>airdrop</Button>
-    <Button>transfer solana</Button>
+    <div className="space-x-4 pt-12">
+        <Button onClick={()=>router.push('/airdrop')}>airdrop</Button>
+        <Button onClick={()=>router.push('/transfer')}>transfer solana</Button>
     </div>    
     </>
 }
